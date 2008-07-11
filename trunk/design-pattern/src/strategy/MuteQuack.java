281e746cd1efe81ffc3a243f676d37c3be05ef83
wangkenn <wangkenn@0adf09dd-ba51-0410-ac10-e74190ecbd62>
2008-07-11 08:26:02 +0000

    first commit

---
+++ trunk/design-pattern/src/strategy/MuteQuack.java
@@ -0,0 +1,10 @@
+package strategy;
+
+public class MuteQuack implements QuackBehavior {
+
+	public void quack() {
+		System.out.println("<< silent >>");
+
+	}
+
+}
---
+++ trunk/design-pattern/src/strategy/Quack.java
@@ -0,0 +1,10 @@
+package strategy;
+
+public class Quack implements QuackBehavior {
+
+	public void quack() {
+		System.out.println("Quack.");
+
+	}
+
+}
